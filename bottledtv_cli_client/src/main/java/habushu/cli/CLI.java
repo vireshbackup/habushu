13e59f033bb964d2754d8cec2122cdf84da405f6
Flurin Rindisbacher <info@flurischt.ch>
2015-02-02 21:09:14 +0100

    add java libraries to gradle

--- bottledtv_cli_client/src/main/java/habushu/cli/CLI.java
+++ bottledtv_cli_client/src/main/java/habushu/cli/CLI.java
@@ -1,10 +1,13 @@
 package habushu.cli;
 
+import com.bottledtv.agent.TheTVDBAgent;
+
 public class CLI
 {
     public static void main(String[] args)
     {
-        System.out.println("machmau");
+        TheTVDBAgent agent = TheTVDBAgent.getInstance();
+        System.out.println("Number of series with this name: " + agent.getSeriesByName("The Big Bang Theory").size());
     }
 }
 
