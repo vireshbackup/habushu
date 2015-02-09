e98188d9160f3c9df7fac58126d99f7dc323a4c1
Flurin Rindisbacher <info@flurischt.ch>
2015-02-09 19:00:35 +0100

    add tvdb_api. implement show-search and episodes list

--- habushu.py
+++ habushu.py
@@ -1,14 +1,29 @@
+import tvdb_api
 from flask import Flask, request
 from flask import render_template
 
+from tvdb_exceptions import tvdb_shownotfound
+
 from collections import namedtuple
 
+# FIXME add our own tvdb api key
+t = tvdb_api.Tvdb(cache = True, banners = False, search_all_languages = True)
 app = Flask(__name__)
 
 Series = namedtuple('Series', ['id', 'language', 'title', 'description'])
 Episodes = namedtuple('Episodes', ['episodeNumber', 'episodeCode', 'seasonNumber', 'localFilePresent', 'firstAired', 'title'])
-TBT = Series(1, 'en', 'The Big Bang Theory', 'test')
-EP1 = Episodes(1, 1, 1, False, '2014-1-1', 'Sheldon\'s Space') 
+
+def _show_as_tuple(show):
+    return Series(int(show['id']), show['language'], show['seriesname'], show['overview'])
+
+def _build_episode_list(tvdb_show):
+    res = []
+    for season in tvdb_show:
+        for e in tvdb_show[season]:
+            episode = tvdb_show[season][e]
+            # FIXME episodeNumber, episodeCode ???
+            res.append(Episodes(int(episode['episodenumber']), int(episode['episodenumber']), int(episode['seasonnumber']), False, episode['firstaired'], episode['episodename']))
+    return res
 
 @app.route('/')
 @app.route('/home')
@@ -18,12 +33,26 @@ def welcome():
 @app.route('/browse')
 def browse():
     search = request.args.get('search', '')
-    return render_template('browse.html', search=search, seriesList=[TBT])
+    seriesList=[]
+    try:
+        # FIXME copying to namedtuple not really necessary... 
+        show = t[search]
+        seriesList.append(_show_as_tuple(show))
+    except tvdb_shownotfound:
+        pass
+    return render_template('browse.html', search=search, seriesList=seriesList)
 
 @app.route('/browse/<int:series_id>/<lang>')
 def episodes(series_id, lang):
-    search = request.args.get('search', '')
-    return render_template('episodes.html', search=search, episodeList=[EP1], series=TBT)
+    show = _show_as_tuple(t[series_id])
+    episodes = _build_episode_list(t[series_id])
+    print episodes
+    return render_template('episodes.html', search=request.args.get('search', ''), episodeList=episodes, series=show)
+
+@app.route('/browse/<int:series_id>/<lang>/<int:season_no>/<int:episode_no>')
+def detail(series_id, lang, season_no, episode_no):
+    return 'Detail page for "%s", Season %i, Episode %i' % (t[series_id]['seriesname'], season_no, episode_no)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
