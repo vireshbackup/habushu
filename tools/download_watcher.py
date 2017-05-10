352f6ce53e53bb9a7df16904605ea345f51c5205
Flurin Rindisbacher <info@flurischt.ch>
2017-05-10 14:05:42 +0200

    fix bug

--- tools/download_watcher.py
+++ tools/download_watcher.py
@@ -36,7 +36,7 @@ def check_torrents(dry_run):
             srt_files = list(filter(lambda f: f['name'].lower().endswith('.srt'), files))
             files_to_move = srt_files + [files[0]]
             files_to_move = map(lambda f: '{}/{}'.format(download_dir, f['name']), files_to_move)
-            directory = os.path.dirname(files_to_move[0]['name'])
+            directory = os.path.dirname(files[0]['name'])
             if directory:
                 target_dir = target_dir + '/' + directory
                 if not os.path.exists(target_dir):
